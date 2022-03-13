3cb74b7eac6d7c62b482ac7aa2a129a0cf4776c8
faithremo <faithmakumi@gmail.com>
2022-03-14 01:24:34 +0300

    generate hash functionn for enhancing password security.

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db
 from .models import User
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+
 auth = Blueprint("auth", __name__)
 
 @auth.route("/login", methods=['GET', 'POST'])
